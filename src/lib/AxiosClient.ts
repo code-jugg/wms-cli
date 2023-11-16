28f066cf3c6020881914b057effa1532768ac1df
code-jugg <codejugg0012@gmail.com>
2023-11-17 03:29:40 +0900

    Update AxiosClient.ts

--- src/lib/AxiosClient.ts
+++ src/lib/AxiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosClient = axios.create({
-  baseURL: process.env.API_BASEURL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     'Content-Type': 'application/json',
   },
