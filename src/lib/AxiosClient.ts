9a7a1fed87dc3b9a8a0cf2d4b9663b39596c1eed
code-jugg <codejugg0012@gmail.com>
2023-11-17 02:39:18 +0900

    Update AxiosClient.ts

--- src/lib/AxiosClient.ts
+++ src/lib/AxiosClient.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const AxiosClient = axios.create({
-  baseURL: "https://wms-api-c3es.onrender.com/api",
+  baseURL: 'https://wms-api-c3es.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
   withCredentials: true,
 });
